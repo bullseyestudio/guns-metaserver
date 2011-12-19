60edfa766188e5b7ab32f33331fd1d76cfd07e94
Octav Sandulescu <github@narc.ro>
2011-12-19 01:53:16 +0000

    Implemented 'actions', sub-pages for the metaserver

--- index.php
+++ index.php
@@ -7,4 +7,17 @@ if(!empty($_GET['args']))
 
 include('inc/config.inc');
 include('inc/sql.inc');
+
+define('PATH_BASE',  dirname(__FILE__));
+define('PATH_ACT',   PATH_BASE.'/act');
+
+$action = 'home';
+if(!empty($_ARGS[0]))
+	$action = $_ARGS[0];
+
+$act_file = PATH_ACT.'/'.$action.'.inc';
+if(!is_file($act_file))
+	die('The action "'.$action.'" cannot be executed. Check your URL.');
+
+include($act_file);
 ?>
